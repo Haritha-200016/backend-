5d724d4ec62e31a7fbc08fcf276277c29e10088e
Haritha <haritha@velastra.co>
2025-07-04 17:46:58 +0530

    updated files after changes

--- sample_backend/app.js
+++ sample_backend/app.js
@@ -43,5 +43,5 @@ app.get('/', (req, res) => {
 
 //Start server
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
